d1b75bbd6c0160e8b31e85585a600addf6ac6021
Stephen James <78153905+ItsMeSteve88@users.noreply.github.com>
2023-03-13 17:43:53 +0000

    cc form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 const App = () =>
 {
+  const handleSubmit = () => {
+    console.log("Form submitted")
+  }
+
+
   return (
-    <div>
-      
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <div className="input-container">
+        <label>Name on Card
+          <input placeholder="Name" required/>
+        </label>
+        </div>
+
+        <div className="input-container">
+        <label>Card Number
+            <input placeholder="0000 0000 0000 0000" minLength={16} maxLength={16} required/>
+        </label>
+        </div>
+
+        <div className="supporting-inputs-container">
+          <label>Expiration Date
+          <input type="month" required/>
+          </label>
+          <label>CVV
+            <input placeholder="123" minLength={3} maxLength={3} required/>
+          </label>
+        </div>
+
+        <div className="input-container">
+          <input type="submit"/>
+        </div>
+
+
+      </form>
     </div>
   )
 }
